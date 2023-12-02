043b6e157ba137acbf54e9e120564a5ae349fae1
Torsten Wylegala <mail@twyleg.de>
2023-12-02 02:21:53 +0100

    Updated init_project.py

--- init_project.py
+++ init_project.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
 import fileinput
+import sys
 from pathlib import Path
 
 FILE_DIR = Path(__file__).parent
+TEMPLATE_NAME = "template_project_python"
+
 
 def replace_string_in_file(filepath: Path, text_to_search: str, replacement_text: str) -> int:
-    with fileinput.FileInput(filepath, inplace=True, backup='.bak') as file:
+    with fileinput.FileInput(filepath, inplace=True) as file:
         for line in file:
             print(line.replace(text_to_search, replacement_text), end='')
 
-def
 
-def replace() -> None:
+def rename(project_name: str) -> None:
+    replace_string_in_file(FILE_DIR / "docs/index.rst", TEMPLATE_NAME, project_name)
+    replace_string_in_file(FILE_DIR / "docs/conf.py", TEMPLATE_NAME, project_name)
+    replace_string_in_file(FILE_DIR / "template_project_python/__main__.py", TEMPLATE_NAME, project_name)
+    replace_string_in_file(FILE_DIR / "template_project_python/main.py", TEMPLATE_NAME, project_name)
+    replace_string_in_file(FILE_DIR / ".gitattributes", TEMPLATE_NAME, project_name)
+    replace_string_in_file(FILE_DIR / "README.rst", TEMPLATE_NAME, project_name)
+    replace_string_in_file(FILE_DIR / "setup.py", TEMPLATE_NAME, project_name)
 
-    template_name = "template_project_python"
-    project_name = "foo_bar"
-
-    replace_string_in_file(FILE_DIR / "docs/index.rst", template_name, project_name)
-    replace_string_in_file(FILE_DIR / "docs/conf.py", template_name, project_name)
-    replace_string_in_file(FILE_DIR / "template_project_python/__main__.py", template_name, project_name)
-    replace_string_in_file(FILE_DIR / "template_project_python/main.py", template_name, project_name)
-    replace_string_in_file(FILE_DIR / ".gitattributes", template_name, project_name)
-    replace_string_in_file(FILE_DIR / "README.rst", template_name, project_name)
-    replace_string_in_file(FILE_DIR / "setup.py", template_name, project_name)
+    Path(FILE_DIR / TEMPLATE_NAME).rename(FILE_DIR / project_name)
 
 
-    Path(FILE_DIR / template_name).rename(FILE_DIR / project_name)
+if __name__ == "__main__":
+    project_name = sys.argv[1]
+    rename(project_name)
+    
